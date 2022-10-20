7baa70f465179c56fd0a7c9f34b8e58b6a96f1f3
Andreas Aaberge Eide <ae@pilco.no>
2022-10-20 15:27:08 +0200

    Finish Knuth's matroid construction algorithm

--- src/knuth74.jl
+++ src/knuth74.jl
@@ -1,18 +1,64 @@
 Family{T} = Set{Set{T}}
 
+
+"""
+Generate a family - a set of sets, each inner set the result of calling
+Set(x), where x is an element in the supplied Vector.
+"""
+function family(set_contents::Vector{Any})::Family{Any}
+  Set([Set([])])
+end
+
+function family(set_contents::Vector{<:Any})::Family{<:Any}
+  Set([Set(e) for e in set_contents])
+end
+
+function pretty_print_family(F)
+  for i in 1:(length(F)-1)
+    println("\n\nF$i")
+    print("{")
+    for s in F[i]
+      for e in s
+        print(e)
+      end
+      print(", ")
+    end
+    print("}\n\n")
+  end
+end
+
 """
   First implementation of Knuth's matroid construction algorithm (1974).
 """
 function knuth_matroid_construction(
-  E::Set{Integer}, 
-  enlargements:: Vector{Vector{Set{<:Any}}}
+  E,
+  enlargements
 )
   # Step 1: Initialize.
-  r = 0 
-  F::Array{Family{Integer}} = [Set(Set())]
+  r = 1
+  F = [family([])]
 
-  # Step 2: Generate covers.
-  
+  while true
+    # Step 2: Generate covers.
+    push!(F, generate_covers(F[r], E))
+
+    # Step 3: Enlarge.
+    if r <= length(enlargements) && enlargements[r] !== nothing
+      F[r+1] = F[r+1] ∪ enlargements[r]
+    end
+
+    # Step 4: Superpose.
+    superpose!(F[r+1], F[r])
+
+    # Step 5: Test for completion.
+    if E ∈ F[r+1]
+      break
+    end
+
+    r += 1
+  end
+
+  return F
 end
 
 """
@@ -23,36 +69,27 @@ function generate_covers(Fr, E)::Family{<:Any}
 end
 
 """
-Enlarge a family by adding some additional sets of elements.
-"""
-function enlarge(family, to_add)::Family{<:Any}
-  family ∪ to_add
-end
-
-"""
   If F_{r+1} contains any two sets A, B whose intersection A ∩ B is not contained in C for any C ∈ F_r, replace A, B ∈ F_{r+1} by the single set A ∪ B. Repeat this operation until A ∩ B ⊆ C for some C ∈ F_r whenever A and B are distinct members of F_{r+1}.
 
-  f_cur and f_old should be Family: A Set of Sets of some type
+  F and F_old should be Family: A Set of Sets of some type
 """
-function superpose(f_cur, f_old)::Family{<:Any}
-  f_new = Set(f_cur)
-
-  for A ∈ f_new
-    for B ∈ f_new
+function superpose!(F, F_old)::Family{<:Any}
+  for A ∈ F
+    for B ∈ F
       should_merge = true
-      for C ∈ f_old
+      for C ∈ F_old
         if A ∩ B ⊆ C
           should_merge = false
         end
       end
 
       if should_merge
-        setdiff!(f_new, [A, B])
-        push!(f_new, A ∪ B)
+        setdiff!(F, [A, B])
+        push!(F, A ∪ B)
       end
     end
   end
 
-  return f_new
+  return F
 end
 
--- test/Test.jl
+++ test/Test.jl
@@ -1,13 +1,6 @@
 using Test
 include("../src/knuth74.jl")
 
-function family(set_contents::Vector{Any})::Family{Any}
-  Set([Set([])])
-end
-
-function family(set_contents::Vector{<:Any})::Family{<:Any}
-  Set([Set(e) for e in set_contents])
-end
 
 @testset "Knuth's matroid construction" begin
   @testset "family helper" begin
@@ -32,23 +25,18 @@ end
       Set([Set([i,j]) for i in ground_set for j in ground_set if i != j])
   end 
 
-  @testset "enlarge" begin
-    @test enlarge(family([1,2,3]), [Set(e) for e in [7,8,9]]) == family([1,2,3,7,8,9])
-  end
-
   @testset "superpose" begin
-    @test superpose(family([1,2,3]), family([])) == family([1,2,3])
+    @test superpose!(family([1,2,3]), family([])) == family([1,2,3])
 
-    # This sets up the example from Knuth (1974) p. 345
+    # This sets up (part of) the example from Knuth (1974) p. 345
     elements = [0,1,2,3,4,5,6,7,8,9]
     ground_set = Set(elements)
     F1 = family(elements)
-    F2 = generate_covers(family(elements), ground_set)
-    F2 = enlarge(F2, family([
+    F2 = generate_covers(family(elements), ground_set) ∪ family([
       [1,3,4], [1,5,9], [2,5,6], [3,5,8], [3,7,9], [2,3,8]]
-    ))
+    )
 
-    @test superpose(F2, F1) == family([
+    @test superpose!(F2, F1) == family([
       [0,1], [0,2], [0,3], [0,4], [0,5], [0,6], [0,7], [0,8], [0,9], 
       [1,2], [1,3,4], [1,5,9], [1,6], [1,7], [1,8], 
       [2,3,5,6,8], [2,4], [2,7], [2,9],
@@ -58,5 +46,42 @@ end
       [6,7], [6,9],
       [7,8],
       [8,9]])
+  end
+
+  @testset "knuth's matroid construction" begin
+    # The example from Knuth (1974) section 3
+    println("KMC")
+    elements = [0,1,2,3,4,5,6,7,8,9]
+    ground_set = Set(elements)
+    enlargements = [nothing, family([
+      [1,3,4], [1,5,9], [2,5,6], [3,5,8], [3,7,9], [2,3,8]]
+    )]
+
+    F1 = family([])
+    F2 = family(elements)
+    F3 = family([
+      [0,1], [0,2], [0,3], [0,4], [0,5], [0,6], [0,7], [0,8], [0,9], 
+      [1,2], [1,3,4], [1,5,9], [1,6], [1,7], [1,8], 
+      [2,3,5,6,8], [2,4], [2,7], [2,9],
+      [3,7,9],
+      [4,5], [4,6], [4,7], [4,8], [4,9],
+      [5,7],
+      [6,7], [6,9],
+      [7,8],
+      [8,9]])
+    F4 = family([
+      [0,1,2], [0,1,3,4], [0,1,5,9], [0,1,6], [0,1,7], [0,1,8],
+      [0,2,3,5,6,8], [0,2,4], [0,2,7], [0,2,9],
+      [0,3,7,9], 
+      [0,4,5], [0,4,6], [0,4,7], [0,4,8], [0,4,9],
+      [0,5,7],
+      [0,6,7], [0,6,9],
+      [0,7,8],
+      [0,8,9],
+      [1,2,3,4,5,6,7,8,9]
+    ])
+    F5 = family([elements])
+
+    @test knuth_matroid_construction(ground_set, enlargements) == [F1, F2, F3, F4, F5]
   end
 end

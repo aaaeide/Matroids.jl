7a1446f2fa0c9a3ba6e2d8b23ea0c40b7733a9db
aaaeide <andreasaaeide@gmail.com>
2023-04-03 16:46:39 +0200

    Add graph viz

--- src/utils.jl
+++ src/utils.jl
@@ -6,3 +6,12 @@ Converts a Set to its bitstring representation.
 function set_to_bits(set, T=UInt64)
   T(sum(2^x for x in set))
 end
+
+# Drawing graphs
+using GraphPlot
+using Compose
+import Fontconfig, Cairo
+
+function draw_graph(g, nodelabel, filename="graph.png")
+  draw(PNG(filename, 32cm, 32cm), gplot(g, nodelabel=nodelabel))
+end
